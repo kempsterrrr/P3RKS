4d2781d780fd9e6a291112f5b7aed651f6aee481
PBillingsby <philbillingsby@gmail.com>
2022-04-22 17:32:22 -0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className='container mx-auto py-4'>
+        <Header />
+        <div className='p-10'>
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
