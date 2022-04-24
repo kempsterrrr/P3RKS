4378098294fa287fc4d6fcf2c21946f3c6a96c52
mbuxmann <mbuxmann@outlook.com>
2022-04-24 20:36:15 +0200

    Fixed home page and added partners

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+const Partners: NextPage = () => {
   const styles = {
     container:
       "p-5 space-y-5 flex flex-col justify-center lg:items-center lg:flex-row lg:space-x-5",
@@ -29,23 +29,25 @@ const Home: NextPage = () => {
         <div className={styles.column}>
           <div className={styles.titleContainer}>
             <h1 className={styles.subtitle}>DROPPING SOON 👀</h1>
-            <h1 className={styles.title}>Benefits for DAO Contributors</h1>
+            <h1 className={styles.title}>
+              Product distribution directly to people building the future
+            </h1>
           </div>
           <p className={styles.info}>
-            Self-service benefits for all the heros and DEGENS out there
-            building the future with frens 🤝
+            Get your product, tool or service directly into the hands of
+            thousands of DAO contributors building the future.
           </p>
           <div className={styles.buttonContainer}>
-            <p>Follow for B3NZ pre-launch alpha!</p>
-            <Link href="https://twitter.com/getb3nz">
-              <a className={styles.button}>@GETB3NZ</a>
+            <p>Drop us your details and we'll be in touch!</p>
+            <Link href="https://airtable.com/shrZZn6ZKZfvrUqDX">
+              <a className={styles.button}>Become a Partner Fren</a>
             </Link>
           </div>
         </div>
         <div>
           <img
             className={styles.image}
-            src="https://www.b3nz.xyz/images/home-page-hero-2.jpg"
+            src="https://www.b3nz.xyz/images/partners-hero.jpeg"
             alt="cyber world"
           />
         </div>
@@ -54,4 +56,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Partners;
