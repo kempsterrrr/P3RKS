7d9635ff8ca5728ed0ee160eca0c4a0a68cd39ec
WIll <kempsterwilliam@gmail.com>
2022-06-26 15:58:17 +0100

    moving Head component to Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { LayoutProps } from "./Layout.d";
 import { useGetUser } from "../../hooks/useGetUser";
 import { NavBar } from "../NavBar";
@@ -7,6 +8,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   useGetUser("/perks");
   return (
     <>
+      <Head>
+        <title>P3RKS | Rewards for DAO Contributors</title>
+        <meta
+          name="description"
+          content="P3RKS | Rewards for DAO Contributors."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <NavBar />
       <div className="flex grow">{children}</div>
       <Footer />
