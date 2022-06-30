d56548ae6ecf903cf9bd6ca75434ec5c3253083a
PBillingsby <philbillingsby@gmail.com>
2022-06-29 17:55:59 -0700

    change b3nz references to p3rks

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-[10px] px-[60px] w-full mx-auto space-x-4 flex justify-center lg:justify-end">
+      <p className="text-[#1A021B] opacity-20 lg:text-[16px]">© 2022 P3RKS</p>
+    </footer>
+  );
+};
+
+export default Footer;
