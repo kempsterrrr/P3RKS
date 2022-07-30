264266c5563c5d04e40a7a028e2d09868843e6ff
WIll <kempsterwilliam@gmail.com>
2022-07-30 12:46:48 +0100

    explicitly siteURL in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || `https://p3rks.xyz`,
+  siteUrl: `https://p3rks.xyz`,
   exclude: ["/perks", "/perks/*"],
   generateRobotsTxt: true,
 };
